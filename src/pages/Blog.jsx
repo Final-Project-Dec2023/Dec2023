ef99637d76ce63bd63130e97fa114d2b06509772
maureen <bisiattah@yahoo.com>
2024-04-25 12:53:07 +0100

    updated

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Jumbotron from '../components/cards/Jumbotron'
+
+const Blog = () => {
+  return (
+    <>
+        <Jumbotron title="Coming Soon..." subTitle="Blog page"/>  
+        </>
+  )
+}
+
+export default Blog
---
+++ src/pages/contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Jumbotron from '../components/cards/Jumbotron'
+
+const Contact = () => {
+  return (
+    <>
+        <Jumbotron title="Coming Soon..." subTitle="contact page"/>  
+        </>
+  )
+}
+
+export default Contact
